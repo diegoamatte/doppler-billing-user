5a9e74d26189d16b80e32753308a2e12a67c7b0d
nreal <nreal@makignsense.com>
2021-10-05 17:39:51 -0300

    feat: add ConsumerType helper

---
+++ Doppler.BillingUser/Utils/ConsumerTypeEnum.cs
@@ -0,0 +1,14 @@
+namespace Doppler.BillingUser.Utils
+{
+    public enum ConsumerTypeEnum
+    {
+        CF = 1,
+        RI = 2,
+        RFC = 4,
+        RNI = 5,
+        MT = 6,
+        EX = 7,
+        NG = 8,
+        NC = 9
+    }
+}
