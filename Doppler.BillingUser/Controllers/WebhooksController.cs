f3567f6e6751a3ab66ad6ef5f972a47a14f5db2d
Diego Amatte <amattediegoomar@gmail.com>
2022-06-08 11:54:07 -0300

    refactor: change logs format

--- Doppler.BillingUser/Controllers/WebhooksController.cs
+++ Doppler.BillingUser/Controllers/WebhooksController.cs
@@ -53,7 +53,7 @@ namespace Doppler.BillingUser.Controllers
             var invoice = await _billingRepository.GetInvoice(user.IdUser, notification.Data.Id.ToString());
             if (invoice is null)
             {
-                _logger.LogError("Invoice with authorization number: {0} was not found.", notification.Data.Id);
+                _logger.LogError("Invoice with authorization number: {authorizationNumber} was not found.", notification.Data.Id);
                 return new NotFoundObjectResult("Invoice not found");
             }
 
@@ -69,7 +69,7 @@ namespace Doppler.BillingUser.Controllers
             {
                 if (invoice.Status == PaymentStatusEnum.Approved)
                 {
-                    _logger.LogError($"The payment associated to the invoiceId {invoice.IdAccountingEntry} was rejected. Reason: {payment.StatusDetail}");
+                    _logger.LogError("The payment associated to the invoiceId {invoiceId} was rejected. Reason: {reason}", invoice.IdAccountingEntry, payment.StatusDetail);
                 }
 
                 await _billingRepository.UpdateInvoiceStatus(invoice.IdAccountingEntry, status);
